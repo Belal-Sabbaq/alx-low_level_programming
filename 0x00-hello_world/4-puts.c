04f223d4d5f6a07e160050d66ea694dbf09fcdad
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-08 02:21:46 +0300

    Betty satisfied finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print a string'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char mystr[52] = "\"Programming is like building a multilingual puzzle";
 
 	puts(mystr);
 	return (0);
-}
+
