cc050ea83ae416c247c58c02ef9eba0bf1aadacb
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-09 02:42:53 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'di'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+/* your code goes there */
+for (a = '0' ; a < '9' + 1  ; a++)
+{
+	putchar(a);
+}
+putchar('\n');
+return (0);
+}
