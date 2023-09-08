397793f0932335824a0542fbefcd9d1d7c559689
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-09 02:27:55 +0300

    alphabey printed

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'di'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a ;
+/* your code goes there */
+for (a = 'a' ; a < 'z'+1  ; a++ )
+	putchar(a);
+putchar('\n');
+return (0);
+}
