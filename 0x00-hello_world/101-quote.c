d5fc1ea20d8dcf35968e746940b486c040b65501
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-08 03:20:13 +0300

    hard core print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: 'prints a string'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+return (0);
+}
