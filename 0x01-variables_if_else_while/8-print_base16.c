25b788a49087cbd1c2ed49d0e90df0132dc39877
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-09 03:51:43 +0300

    HEX done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'di'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+/* your code goes there */
+for (a = 48; a < 58  ; a++)
+{
+	putchar((char)a);
+}
+for (a = 97; a < 103 ; a++)
+{
+	putchar((char)a);
+}
+putchar('\n');
+return (0);
+}
