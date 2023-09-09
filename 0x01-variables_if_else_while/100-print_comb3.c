4bd9aea1b17fd513b4afa26687c5ad2860d7580f
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-09 04:08:34 +0300

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'di'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+int b;
+/* your code goes there */
+for (a = 48; a < 58  ; a++)
+{
+	for (b = 48; b < 58 ; b++)
+	{
+        	if (a >= b)
+		{
+		continue;
+		}
+		putchar((char)a);
+		putchar((char)b);
+	        if (a == 56 && b == 57)
+			continue;
+		putchar(',');
+        	putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
