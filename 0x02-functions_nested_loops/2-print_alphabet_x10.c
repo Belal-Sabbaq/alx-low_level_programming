f34ccbef57434a1dee69e3502269966ee9d73f89
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-13 02:22:27 +0300

    peint 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 11; i++)
+		void print_alphabet(void);
+}
