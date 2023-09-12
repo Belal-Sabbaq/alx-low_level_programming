e98c8940648a2d76b539f05ddc0276ee8840a1d6
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-12 20:13:32 +0300

    printf function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'di'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* your code goes there */
+printf("_putchar\n");
+return (0);
+}
