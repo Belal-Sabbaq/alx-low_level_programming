0607fe2fb2dee0d86daaec6a14bb405aa4175629
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-13 03:38:30 +0300

    longest code in my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - longest day in my life
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+			if (i == 2 && j > 3)
+				break;
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar('i');
+					_putchar('j');
+					_putchar(':');
+					_putchar('k');
+					_putchar('l');
+					_putchar('\n');
+				}
+
+			}
+		}
+
+	}
+}
