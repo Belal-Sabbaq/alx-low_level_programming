c93b2d7bd0635a60d0b019393b61b04da5121f6e
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-13 04:23:52 +0300

    add 2 digits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two digits
+ *
+ * @a: checks firdt digit input of the function
+ *
+ * @b: checks secons digit input of the function
+ *
+ * Return: returns the sum of the two inputs
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
