48c06fe0fb98fee4c8c6775be87da613197ff3f6
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-13 04:33:34 +0300

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print till we get to '98'
+ *
+ * @n: number we start from
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("\d, ", n);
+		if (n > 98)
+			n--
+		else
+			n++
+	}
+	putchar('9');
+	putchar('8');
+	putchar('\n');
+}
