ac2ac847faeac66b794f1020125fb2036d0164d7
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-13 04:03:39 +0300

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+i#include "main.h"
+
+/**
+ * timetable - time table from '1' to '9'
+ */
+
+void times_table(void);
+{
+	int i, j, f1, f2;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == 0 && j == 0)
+			{
+				_putchar(48);
+				_putchar(',');
+			}
+			if (i *j > 9)
+			{
+				f2 = (i *j) % 10;
+				f1 = ((i *j) - f2) / 10;
+				_putchar(' ');
+				_putchar(f1 + 48);
+				_putchar(f2 + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((i *j) + 48);
+
+			}
+		}
+		_putchar('\n');
+	}
+}
