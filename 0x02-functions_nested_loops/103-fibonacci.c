2396a93e533c973f6db7c1320866761cda547167
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-13 05:39:57 +0300

    fibo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,19 +7,18 @@
  */
 int main(void)
 {
-	int n1, n2, head, c;
+	int n1, n2, head, sum;
 /* your code goes there */
 	n1 = 1;
 	n2 = 2;
 	c = 2;
 	printf("1, 2, ");
-	while(c < 50)
+	while(sum < )
 	{
 	head = n1 + n2;
 	printf("%d, ", head);
 	n1 = n2;
 	n2 = head;
-	c++;
 	}
 	printf("%d\n", n1 + n2);
 	return (0);
