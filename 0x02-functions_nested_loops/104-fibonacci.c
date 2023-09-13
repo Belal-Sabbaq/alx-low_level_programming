9b0f6ab0f385b038715428ac3ba7ce5f31ee834b
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-13 06:00:27 +0300

    fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: fibo
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long n1, n2, head, c;
+/* your code goes there */
+	n1 = 1;
+	n2 = 2;
+	c = 2;
+	printf("1, 2, ");
+	while (c < 97)
+	{
+	head = n1 + n2;
+	printf("%ld, ", head);
+	n1 = n2;
+	n2 = head;
+	c++;
+	}
+	printf("%ld\n", n1 + n2);
+	return (0);
+}
