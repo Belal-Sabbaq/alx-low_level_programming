8c3dd37c5b15fda75ecf3ff8ad4b0738aae866d4
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-19 01:00:39 +0300

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change variable value without return
+ *@n: pointer to the variable that was desired to be changed
+ */
+
+void reset_to_98(int* n)
+{
+	*n = 98;
+}
