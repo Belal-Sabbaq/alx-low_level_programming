1d6283136577846245a2b58be93d2031aca65b0e
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-19 01:28:13 +0300

    string output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - output chars and strings
+ * @str: pointer to the first element
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+}
