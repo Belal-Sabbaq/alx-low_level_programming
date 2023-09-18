11c81716f0e88ad9c539218424c58c269ee8f730
Belal_Sabbaq <belal_sabbaq@UBUNTU.myguest.virtualbox.org>
2023-09-19 02:06:55 +0300

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,14 @@ void puts_half(char *str)
 /**after declration */
 	c = 0;
 	i = 0;
-	while (*(s + i) != '\0')
+	while (*(str + i) != '\0')
 	{
 		c++;
 		i++;
 	}
 	for (j = ((c - 1) / 2); j < c; j++)
 	{
-		_putchar(*(s + j));
+		_putchar(*(str + j));
 	}
 	_putchar('\n');
 }
